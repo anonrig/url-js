1ab1e2142188035231f3af78241ce9c3be80632e
Yagiz Nizipli <yagiz@nizipli.com>
2022-03-06 20:43:54 -0500

    perf: add url-state-machine to benchmark

--- benchmark.js
+++ benchmark.js
@@ -1,8 +1,11 @@
+const URLStateMachine = require('./lib')
 const Benchmarkify = require('benchmarkify')
 const bench = new Benchmarkify('URL').printHeader()
 
 const suite = bench.createSuite('url')
 
 suite.add('URL', () => new URL('https://www.google.com/path/to/something'))
+suite.add('url-state-machine', () => new URLStateMachine('https://www.google.com/path/to/something'))
+
 
 bench.run([suite])
