5aa7b76ddeb5ecf8b73455db079e5a136ad4561e
Yagiz Nizipli <yagiz@nizipli.com>
2022-03-22 17:38:14 -0400

    refactor: update compliance tests

--- test/coverage.test.ts
+++ test/coverage.test.ts
@@ -1,18 +1,26 @@
 import { test, assert } from 'vitest'
-import tests from './coverage.json'
+import suites from './coverage.json'
 import StateMachine from '../lib'
 
-for (let automated_test of tests) {
-  if (typeof automated_test === 'string') {
+type Suite = {
+  input: string
+  base?: string
+  protocol?: string
+}
+
+for (let suite of suites) {
+  if (typeof suite === 'string') {
     continue
   }
 
-  if (automated_test.input && automated_test.base) {
-    test(automated_test.input, () => {
-      const instance = new StateMachine(automated_test.input, automated_test.base)
+  if (suite.input) {
+    const t = suite as Suite
 
-      if (automated_test.protocol) {
-        assert.equal(instance.url.scheme, automated_test.protocol.replaceAll(':', ''))
+    test(t.input, () => {
+      const state = new StateMachine(t.input, t.base)
+
+      if (t.protocol) {
+        assert.equal(state.url.scheme, t.protocol.replaceAll(':', ''))
       }
     })
   }
