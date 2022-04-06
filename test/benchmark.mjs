1ca8fa3718cbf1e52c25c683a34239eff4f9e981
Yagiz Nizipli <yagiz@nizipli.com>
2022-04-05 20:37:06 -0400

    feat: introduce rome formatter

--- test/benchmark.mjs
+++ test/benchmark.mjs
@@ -1,15 +1,18 @@
-import URLStateMachine from '../lib/index.js'
-import WHATWG from 'whatwg-url'
-import benchmark from 'cronometro'
+import URLStateMachine from "../lib/index.js";
+import WHATWG from "whatwg-url";
+import benchmark from "cronometro";
 
-await benchmark({
-  'URL'() {
-    return new URL('https://www.google.com/path/to/something')
+await benchmark(
+  {
+    "URL"() {
+      return new URL("https://www.google.com/path/to/something");
+    },
+    "url-state-machine"() {
+      return new URLStateMachine("https://www.google.com/path/to/something");
+    },
+    "whatwg-url"() {
+      return new WHATWG.URL("https://www.google.com/path/to/something");
+    },
   },
-  'url-state-machine'() {
-    return new URLStateMachine('https://www.google.com/path/to/something')
-  },
-  'whatwg-url'() {
-    return new WHATWG.URL('https://www.google.com/path/to/something')
-  }
-}, { warmup: true })
+  { warmup: true },
+);
--- test/coverage.test.ts
+++ test/coverage.test.ts
@@ -1,47 +1,67 @@
 // load tests from https://github.com/web-platform-tests/wpt/blob/master/url/resources/urltestdata.json
-import { test, assert } from 'vitest'
-import url_test_data from './urltestdata.json'
-import StateMachine from '../lib'
+import { test, assert } from "vitest";
+import path from "node:path";
+import url_test_data from "./urltestdata.json";
+import StateMachine from "../lib";
 
 type Suite = {
-  input: string
-  base?: string
-  protocol?: string
-  failure?: boolean
-  username?: string
-  password?: string
-}
+  input: string,
+  base?: string,
+  protocol?: string,
+  failure?: boolean,
+  username?: string,
+  password?: string,
+  host?: string,
+  hostname?: string,
+  fragment?: string,
+};
 
 for (let suite of url_test_data) {
-  if (typeof suite === 'string') {
-    continue
+  if (typeof suite === "string") {
+    continue;
   }
 
-  if (suite.input) {
-    const t = suite as Suite
+  const t = suite as Suite;
 
-    test(t.input, () => {
-      try {
-        const state = new StateMachine(t.input, t.base)
+  if (suite.input) {
+    const url = path.join(t.input, t.base ?? "");
+    test(
+      url,
+      () => {
+        const state = new StateMachine(url);
+
+        if (typeof t.failure !== "undefined") {
+          assert.equal(state.failure, t.failure);
+        }
 
         if (t.protocol) {
-          assert.equal(state.url.scheme, t.protocol.replaceAll(':', ''))
+          assert.equal(
+            state.url.scheme,
+            t.protocol.replaceAll(":", ""),
+            JSON.stringify(state, null, 2),
+          );
         }
 
         if (t.password) {
-          assert.equal(state.url.password, t.password)
+          assert.equal(state.url.password, t.password);
         }
 
         if (t.username) {
-          assert.equal(state.url.username, t.username)
+          assert.equal(state.url.username, t.username);
         }
-      } catch (error) {
-        if (t.failure) {
-          assert(error.message)
-        } else {
-          throw error
+
+        if (t.host) {
+          assert.equal(state.url.host, t.host, JSON.stringify(state, null, 2));
         }
-      }
-    })
+
+        if (t.fragment) {
+          assert.equal(
+            state.url.fragment,
+            t.fragment,
+            JSON.stringify(state, null, 2),
+          );
+        }
+      },
+    );
   }
 }
